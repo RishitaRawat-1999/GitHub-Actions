b269d90399dfe4b865067318ffd7a32249b97c1c
Rishita Rawat <72519591+RishitaRawat-1999@users.noreply.github.com>
2026-06-08 12:12:00 +0530

    Update addition.py
    Added subtration

--- src/addition.py
+++ src/addition.py
@@ -7,3 +7,10 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+#Added substration
+def sub(c, d):
+    return c - d
+
+def test_sub():
+    assert sub(5, 2) == 3
